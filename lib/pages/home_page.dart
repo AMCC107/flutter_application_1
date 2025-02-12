8b7ad0b2a75b708d690e3d20101b5d6e708b5d32
AMCC107 <mauriciocc11@hotmail.com>
2025-02-12 13:24:58 -0600

    Main Text Added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -7,18 +7,48 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class  _HomePageState extends State <HomePage> {
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
-        //Icono izquierdo
-        leading: Icon (Icons.menu,color: Colors.grey[800]),
-        actions:[
-          Icon(Icons.person)
+        // Icono izquierdo
+        leading: Icon(Icons.menu, color: Colors.grey[800]),
+        actions: [
+          const Padding(
+            padding: EdgeInsets.only(right: 24.0),
+            child: Icon(Icons.person),
+          ),
+        ],
+      ),
+      body: const Column(
+        children: [
+          // Texto Principal
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal:36,vertical:18),
+            child: Row(
+              children: [
+                Text("I want to ", 
+                style:TextStyle(fontSize : 32)),
+                Text(
+                  "Eat",
+                  style: TextStyle(
+                    // Tamaño de letra
+                    fontSize: 32.0,
+                    // Negritas
+                    fontWeight: FontWeight.bold,
+                    // Subrayado
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          // 
         ],
       ),
     );
   }
 }
+
